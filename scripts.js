76d81a06d41fd9818b0d00b59ddd414ef167fee5
NAO <naokosaka17@gmail.com>
2017-08-08 16:36:29 -0400

    make play and pause button

---
+++ scripts.js
@@ -0,0 +1,33 @@
+//-- Get elements --
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipbuttons = player.querySelector('[data-skip]');
+const range = player.querySelector('.player__slider');
+
+//-- functions --
+// make play and pause video (es5)
+function toggleplay(){
+  if(video.paused){
+    video.play();
+  }else{
+    video.pause();
+  }
+}
+
+// make update play and pause buttons
+function updateButton(){
+  // console.log("update button");
+  // this === video
+  const icon = this.paused ? '▶' : '▮▮';
+  toggle.textContent = icon;
+}
+
+//-- event listeners --
+// when click on the screen function toggleplay is started
+video.addEventListener('click', toggleplay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', toggleplay);
